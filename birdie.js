1c31b43a17380095ecf84e84c38dfbc7695e0add
dcky <dcky@users.noreply.github.com>
2017-02-04 19:04:21 -0600

    Update birdie.js

--- birdie.js
+++ birdie.js
@@ -104,11 +104,11 @@ function checkOverlap(body1, body2) {
 
 function update() {
 	this.player.body.velocity.x = 0;
-    if (this.cursors.left.isDown && this.cursors.right.isDown) {
+    //if (this.cursors.left.isDown && this.cursors.right.isDown) {
 			//this.player.body.velocity.x = 0;
 		
 		//} else if (this.cursors.left.isDown)
-    } else if (this.cursors.left.isDown) {
+    if (this.cursors.left.isDown) {
         this.player.body.velocity.x = -200;
 		
 		 if (this.facing != 'left')
