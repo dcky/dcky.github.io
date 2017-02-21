7a860a72c37cf0096ce3f3d87b4fc5ec160915a6
dcky <dcky@users.noreply.github.com>
2017-02-20 22:41:03 -0600

    Update birdie.js

--- birdie.js
+++ birdie.js
@@ -1,33 +1,35 @@
+var game = new Phaser.Game(800, 300, Phaser.AUTO, 'birdie', { preload: preload, create: create, update: update, render: render });
+var Game = {}
+	
+Game.win = function(game) {
 
-var win = {
-		
-		create: function() {
-        var winLabel = game.add.text(80,80,'YOU WON!',{font:'50px Arial', fill:'#FFF'});
-        var startLabel = game.add.text(80,game.world.height-80,'Press "w" to restart the game', {font:'25px Arial', fill:'#FFF'});
-
-        var wkey = game.input.keyboard.addKey(Phaser.Keyboard.W);
-        wkey.onDown.addOnce(this.restart,this);
-		}
 }
 
-var game = new Phaser.Game(800, 300, Phaser.AUTO, 'birdie', { preload: preload, create: create, update: update, render: render });
+Game.win.prototype = {
+	update: function() { 
+		console.log("hi");
+			this.text = game.add.text(200, 200, 'You win', { font: "32px Arial", fill: "#ffffff", align: "center" });
+    this.text.anchor.setTo(0.5, 0.5);
+	}
 
-//game.state.add('preload', preload);
-//game.state.add('create', create);
-//game.state.add('play', play);
-game.state.add('win', win);
+}	
 
-//game.state.start('preload');
+
+
+game.state.add('win', Game.win);
+
 
 function preload() {
 
 	game.load.crossOrigin = "Anonymous";
 	game.load.image('coin','assets/dcoin.png');
 	game.load.image('ground', 'assets/ground.png');
-	game.load.image('background','assets/background.png')
+	game.load.image('background','assets/background.png');
+	game.load.image('anvil','assets/anvil.png');
 	game.load.spritesheet('duck_animation','assets/duck_animation.png', 47, 39);
 }
 
+var emitter;
 var player;
 var ground;
 var cursors;
@@ -39,11 +41,15 @@ var maxCoins;
 var coinGroup;
 var coinsOnScreen;
 var text;
+var lost = false;
+var objs = [];
+var coins = [];
 
 function create() {
 	this.coinsOnScreen = 0;
 	this.maxCoins = 3;
 	this.kuai = 0;
+	this.lost = false;
 
 
     game.physics.startSystem(Phaser.Physics.P2JS);
@@ -63,12 +69,12 @@ function create() {
     
     this.player.body.fixedRotation = true;
 	
-	
 	this.ground = game.add.sprite(400, 282, 'ground');
 	this.ground.name = 'ground';
 	game.physics.p2.enable(this.ground);
 	
 	game.time.events.loop(750, spawnCoin,  this);
+    game.time.events.add(500, startObj, this);
 	
 	this.text = game.add.text(72, 25, 'Score: 0', { font: "32px Arial", fill: "#ffffff", align: "center" });
     this.text.anchor.setTo(0.5, 0.5);
@@ -82,6 +88,7 @@ function create() {
 }
 
 function checkOverlap(body1, body2) {
+	if (this.lost == false) {
     if ((body1.sprite.name == 'player' && body2.sprite.name == 'coin') || (body2.sprite.name == 'player' && body1.sprite.name == 'coin')){
         
 		if (body1.sprite.name == 'coin') {
@@ -89,30 +96,47 @@ function checkOverlap(body1, body2) {
 			this.coinsOnScreen -= 1;
 			this.kuai += 1;
 			this.text.setText('Score: ' + this.kuai);
-			if (this.kuai > 2) {
-					game.state.start('win');
-			}
 		} else if (body2.sprite.name == 'coin') {
 			body2.sprite.kill();
 			this.coinsOnScreen -= 1;
 			this.kuai += 1;
 			this.text.setText('Score: ' + this.kuai);
-			if (this.kuai > 2) {
-					game.state.start('win');
-			}			
 		}
 		
         return false;
+    }
+	
+	if ((body1.sprite.name == 'player' && body2.sprite.name == 'bad') || (body2.sprite.name == 'player' && body1.sprite.name == 'bad')){
+        
+		this.lost = true;
+		
+		return false;
+
     }
 	
     if ((body1.sprite.name == 'ground' && body2.sprite.name == 'coin') || (body2.sprite.name == 'ground' && body1.sprite.name == 'coin')){
         
 		if (body1.sprite.name == 'coin') {
 			body1.sprite.kill();
+			coins.splice(0, 1);
 			this.coinsOnScreen -= 1;
 		} else if (body2.sprite.name == 'coin') {
 			body2.sprite.kill();
+			coins.splice(0, 1);
 			this.coinsOnScreen -= 1;			
+		}
+		
+        return false;
+    }
+
+    if ((body1.sprite.name == 'ground' && body2.sprite.name == 'bad') || (body2.sprite.name == 'ground' && body1.sprite.name == 'bad')){
+        
+		if (body1.sprite.name == 'bad') {
+			body1.sprite.kill();
+			objs.splice(0, 1);
+		} else if (body2.sprite.name == 'bad') {
+			body2.sprite.kill();	
+			objs.splice(0, 1);
 		}
 		
         return false;
@@ -123,11 +147,12 @@ function checkOverlap(body1, body2) {
     }	
 	
     return true;
+	}
 }
 
 function update() {
 	this.player.body.velocity.x = 0;
-
+	if (this.lost == false) {
     if (this.cursors.left.isDown && this.cursors.right.isUp) {
         this.player.body.velocity.x = -300;
 		
@@ -170,6 +195,39 @@ function update() {
                 this.player.frame = 0;
             }		
 	}
+	
+	} else {
+		this.game.physics.p2.applyGravity = false
+		this.player.body.velocity.x = 0;
+		this.player.body.velocity.y = 0;
+	    game.add.tween(this.player).to( { alpha: 0 }, 1000, "Quart.easeOut", true);
+
+			this.text = game.add.text(400, 150, 'You lost!', { font: "32px Arial", fill: "#ffffff", align: "center" });
+			this.text.alpha = 0;
+			game.add.tween(this.text).to( { alpha: 1 }, 1000, "Quart.easeIn", true);
+		this.text.anchor.setTo(0.5, 0.5);
+	
+		for (i = 0; i < objs.length; i++)  {
+			o = objs[i];
+			console.log(o);
+			
+					o.body.velocity.x = 0;
+				o.body.velocity.y = 0;
+				game.add.tween(o).to( { alpha: 0 }, 1000, "Quart.easeOut", true);
+		}
+		
+
+		
+		for (i = 0; i < coins.length; i++) {
+			c = coins[i];
+								c.body.velocity.x = 0;
+				c.body.velocity.y = 0;
+				game.add.tween(c).to( { alpha: 0 }, 1000, "Quart.easeOut", true);
+		}
+		
+		coins = []
+				objs = []
+	}
 
 	
 	
@@ -184,7 +242,9 @@ function update() {
 	
 }
 
+
 function spawnCoin() {
+	if (this.lost == false) {
 			        coin = game.add.sprite(this.game.rnd.integerInRange(20, this.game.width-20), 16, 'coin');
 				
 				coin.name = "coin";
@@ -193,18 +253,28 @@ function spawnCoin() {
         
 		game.physics.p2.enable(coin);
 		coin.body.setCircle(16);
-	
+		coins.push(coin);
+	}
 }
 
-
-
-function restart() {
-    game.state.start('create');
-
+function spawnObj() {
+	if (this.lost == false) {
+			        obj = game.add.sprite(this.game.rnd.integerInRange(20, this.game.width-20), 7, 'anvil');
+				
+				obj.name = "bad";
+        
+		game.physics.p2.enable(obj);
+		obj.body.setRectangle(32,28);
+		
+		objs.push(obj);
+		//console.log(objs);
+	}
 }
+
+function startObj() {
+	game.time.events.loop(750, spawnObj,  this);
+}	
 	
-
-
 function render() {
 
 }
