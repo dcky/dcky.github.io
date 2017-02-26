acc63951173aacad582f46b8ccc45fc6f7f5a977
dcky <dcky@users.noreply.github.com>
2017-02-25 23:10:02 -0600

    Update birdie.js

--- birdie.js
+++ birdie.js
@@ -1,23 +1,4 @@
 var game = new Phaser.Game(800, 300, Phaser.AUTO, 'birdie', { preload: preload, create: create, update: update, render: render });
-var Game = {}
-	
-Game.win = function(game) {
-
-}
-
-Game.win.prototype = {
-	update: function() { 
-		console.log("hi");
-			this.text = game.add.text(200, 200, 'You win', { font: "32px Arial", fill: "#ffffff", align: "center" });
-    this.text.anchor.setTo(0.5, 0.5);
-	}
-
-}	
-
-
-
-game.state.add('win', Game.win);
-
 
 function preload() {
 
@@ -27,6 +8,7 @@ function preload() {
 	game.load.image('background','assets/background.png');
 	game.load.image('anvil','assets/anvil.png');
 	game.load.spritesheet('duck_animation','assets/duck_animation.png', 47, 39);
+	this.game.load.bitmapFont('Pixel', 'assets/Pixel.png', 'assets/Pixel.fnt');
 }
 
 var emitter;
@@ -34,7 +16,7 @@ var player;
 var ground;
 var cursors;
 var jumpButton;
-var facing = 'idle';
+var facing;
 var kuai;
 var background;
 var maxCoins;
@@ -44,72 +26,78 @@ var text;
 var lost = false;
 var objs = [];
 var coins = [];
+var dObj;
+var dCoin;
+var completed = false;
+var lostText;
+var restartText;
 
 function create() {
-	this.coinsOnScreen = 0;
-	this.maxCoins = 3;
-	this.kuai = 0;
-	this.lost = false;
-
-
     game.physics.startSystem(Phaser.Physics.P2JS);
 
 	background = game.add.tileSprite(0, 0, 800, 300, 'background');
 	
     game.physics.p2.gravity.y = 500;
+
 	
-	this.player = game.add.sprite(100, 100, 'duck_animation');
-	this.player.smoothed = false;
-	this.player.name = 'player';
-	this.player.animations.add('right', [1, 0], 5, true);
-	this.player.animations.add('left', [2, 3], 5, true);
+	player = game.add.sprite(100, 100, 'duck_animation');
+	player.smoothed = false;
+	player.name = 'player';
+	player.animations.add('right', [1, 0], 5, true);
+	player.animations.add('left', [2, 3], 5, true);
+	player.alpha = 0;
 	
-    game.physics.p2.enable(this.player);
-	console.log(this.player.name);
+    game.physics.p2.enable(player);
     
-    this.player.body.fixedRotation = true;
+    player.body.fixedRotation = true;
 	
 	this.ground = game.add.sprite(400, 282, 'ground');
 	this.ground.name = 'ground';
+	this.ground.immovable = true;
 	game.physics.p2.enable(this.ground);
 	
 	game.time.events.loop(750, spawnCoin,  this);
     game.time.events.add(500, startObj, this);
 	
-	this.text = game.add.text(72, 25, 'Score: 0', { font: "32px Arial", fill: "#ffffff", align: "center" });
-    this.text.anchor.setTo(0.5, 0.5);
+	text = game.add.bitmapText(72, 25, 'Pixel', 'Score: 0', 16);
+	
+    text.anchor.setTo(0.5, 0.5);
 	
 	
     this.cursors = game.input.keyboard.createCursorKeys();
     this.jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    this.restartButton = game.input.keyboard.addKey(Phaser.Keyboard.R);
 	
 	game.physics.p2.setPostBroadphaseCallback(checkOverlap, this);
-
+	
+	lost = false;
+	this.completed = false;
+	
+	resetEverything();
 }
 
 function checkOverlap(body1, body2) {
-	if (this.lost == false) {
+		
     if ((body1.sprite.name == 'player' && body2.sprite.name == 'coin') || (body2.sprite.name == 'player' && body1.sprite.name == 'coin')){
         
 		if (body1.sprite.name == 'coin') {
 			body1.sprite.kill();
-			this.coinsOnScreen -= 1;
-			this.kuai += 1;
-			this.text.setText('Score: ' + this.kuai);
+			coinsOnScreen -= 1;
+			kuai += 1;
+			text.setText('Score: ' + kuai);
 		} else if (body2.sprite.name == 'coin') {
 			body2.sprite.kill();
-			this.coinsOnScreen -= 1;
-			this.kuai += 1;
-			this.text.setText('Score: ' + this.kuai);
+			coinsOnScreen -= 1;
+			kuai += 1;
+			text.setText('Score: ' + kuai);
 		}
 		
         return false;
     }
 	
 	if ((body1.sprite.name == 'player' && body2.sprite.name == 'bad') || (body2.sprite.name == 'player' && body1.sprite.name == 'bad')){
-        
-		this.lost = true;
-		
+		lost = true;
+		this.completed = false
 		return false;
 
     }
@@ -119,11 +107,11 @@ function checkOverlap(body1, body2) {
 		if (body1.sprite.name == 'coin') {
 			body1.sprite.kill();
 			coins.splice(0, 1);
-			this.coinsOnScreen -= 1;
+			coinsOnScreen -= 1;
 		} else if (body2.sprite.name == 'coin') {
 			body2.sprite.kill();
 			coins.splice(0, 1);
-			this.coinsOnScreen -= 1;			
+			coinsOnScreen -= 1;			
 		}
 		
         return false;
@@ -147,91 +135,112 @@ function checkOverlap(body1, body2) {
     }	
 	
     return true;
-	}
+	//}
 }
 
 function update() {
-	this.player.body.velocity.x = 0;
-	if (this.lost == false) {
+	
+	player.body.velocity.x = 0;
+	if (lost == false) {
     if (this.cursors.left.isDown && this.cursors.right.isUp) {
-        this.player.body.velocity.x = -300;
+        player.body.velocity.x = -300;
 		
-		 if (this.facing != 'left')
+		 if (facing != 'left')
         {
-            this.player.animations.play('left');
-            this.facing = 'left';
+            player.animations.play('left');
+            facing = 'left';
         }
 
     }
     else if (this.cursors.right.isDown && this.cursors.left.isUp)
     {
-        this.player.body.velocity.x = 300;
-		if (this.facing != 'right')
+        player.body.velocity.x = 300;
+		if (facing != 'right')
         {
-            this.player.animations.play('right');
-            this.facing = 'right';
+            player.animations.play('right');
+            facing = 'right';
         }
     }
     else
     {
-        this.player.body.velocity.x = 0;
+        player.body.velocity.x = 0;
     }
 	
 	if (this.jumpButton.isDown) {
-		this.player.body.velocity.y = -200;
-		if (this.facing === 'right') {
-            this.player.animations.play('right');
+		player.body.velocity.y = -200;
+		if (facing === 'right') {
+            player.animations.play('right');
         } else {
-			this.player.animations.play('left');
+			player.animations.play('left');
 		}
 		
 		
-		//this.facing = 'idle';
-	} else if (this.player.body.y > 243) {
-		this.player.animations.stop();
-		if (this.facing == 'left') {
-                this.player.frame = 3;
+	} else if (player.body.y > 243) {
+		player.animations.stop();
+		if (facing == 'left') {
+                player.frame = 3;
             } else {
-                this.player.frame = 0;
+                player.frame = 0;
             }		
 	}
 	
 	} else {
-		this.game.physics.p2.applyGravity = false
-		this.player.body.velocity.x = 0;
-		this.player.body.velocity.y = 0;
-	    game.add.tween(this.player).to( { alpha: 0 }, 1000, "Quart.easeOut", true);
+		if (this.completed == false) {
+		this.completed = true;
+		this.game.physics.p2.applyGravity = false;
+		player.body.velocity.x = 0;
+		player.body.velocity.y = 0;
+	    game.add.tween(player).to( { alpha: 0 }, 1000, "Quart.easeOut", true);
 
-			this.text = game.add.text(400, 150, 'You lost!', { font: "32px Arial", fill: "#ffffff", align: "center" });
-			this.text.alpha = 0;
-			game.add.tween(this.text).to( { alpha: 1 }, 1000, "Quart.easeIn", true);
-		this.text.anchor.setTo(0.5, 0.5);
-	
-		for (i = 0; i < objs.length; i++)  {
-			o = objs[i];
-			console.log(o);
+			lostText = game.add.bitmapText(400, 150, 'Pixel', 'You lost!', 16);
+			restartText = game.add.bitmapText(400, 180, 'Pixel', 'Press \'R\' to restart', 8);
+						lostText.anchor.setTo(0.5, 0.5);
+			restartText.anchor.setTo(0.5, 0.5);
 			
-					o.body.velocity.x = 0;
-				o.body.velocity.y = 0;
-				game.add.tween(o).to( { alpha: 0 }, 1000, "Quart.easeOut", true);
+			lostText.alpha = 0;
+			restartText.alpha = 0;
+			game.add.tween(lostText).to( { alpha: 1 }, 1000, "Quart.easeIn", true);
+			game.add.tween(restartText).to( { alpha: 1 }, 1000, "Quart.easeIn", true);
+
+		for (i = 0; i < objs.length; i++)  {
+				objs[i].body.velocity.y = 0;
+				objs[i].body.velocity.x = 0;
+				objs[i].name = "lol";
+				objs[i].body.data.shapes[0].sensor=true;
+				game.add.tween(objs[i]).to( { alpha: 0 }, 1000, "Quart.easeOut", true);
 		}
 		
 
 		
 		for (i = 0; i < coins.length; i++) {
-			c = coins[i];
-								c.body.velocity.x = 0;
-				c.body.velocity.y = 0;
-				game.add.tween(c).to( { alpha: 0 }, 1000, "Quart.easeOut", true);
+			
+				coins[i].body.velocity.y = 0;
+				coins[i].body.velocity.x = 0;
+				coins[i].name = "lol";
+				coins[i].body.data.shapes[0].sensor=true;
+		
+				game.add.tween(coins[i]).to( { alpha: 0 }, 1000, "Quart.easeOut", true);
+				
 		}
 		
+		this.completed = true;
 		coins = []
-				objs = []
+		objs = []
+		}
+		
+		if (this.restartButton.isDown) {
+				lost = false;
+				game.add.tween(lostText).to( { alpha: 0 }, 1000, "Quart.easeOut", true);
+				game.add.tween(restartText).to( { alpha: 0 }, 1000, "Quart.easeOut", true);
+				
+				
+				resetEverything();
+		}
 	}
 
 	
 	
-	    if (this.coinsOnScreen < this.maxCoins) {
+	    if (coinsOnScreen < maxCoins) {
         // Set the launch point to a random location below the bottom edge
         // of the stage
 
@@ -244,12 +253,12 @@ function update() {
 
 
 function spawnCoin() {
-	if (this.lost == false) {
+	if (lost == false) {
 			        coin = game.add.sprite(this.game.rnd.integerInRange(20, this.game.width-20), 16, 'coin');
 				
 				coin.name = "coin";
 
-		this.coinsOnScreen += 1;
+		coinsOnScreen += 1;
         
 		game.physics.p2.enable(coin);
 		coin.body.setCircle(16);
@@ -258,7 +267,7 @@ function spawnCoin() {
 }
 
 function spawnObj() {
-	if (this.lost == false) {
+	if (lost == false) {
 			        obj = game.add.sprite(this.game.rnd.integerInRange(20, this.game.width-20), 7, 'anvil');
 				
 				obj.name = "bad";
@@ -267,7 +276,6 @@ function spawnObj() {
 		obj.body.setRectangle(32,28);
 		
 		objs.push(obj);
-		//console.log(objs);
 	}
 }
 
@@ -275,6 +283,22 @@ function startObj() {
 	game.time.events.loop(750, spawnObj,  this);
 }	
 	
-function render() {
-
+function resetEverything() {
+	player.animations.stop();
+	player.frame = 0;
+	facing = "right";
+	game.add.tween(player).to( { alpha: 1 }, 500, "Quart.easeIn", true);
+	
+	player.body.x = 100;
+	player.body.y = 100;
+	
+	game.physics.p2.applyGravity = true;
+		
+	coinsOnScreen = 0;
+	maxCoins = 3;
+	kuai = 0;
+	lost = false;
+	
 }
+
+function render() {}
